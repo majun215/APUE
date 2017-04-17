e31f1a7f8f56f37f6bb17df98949c65a1077bd2f
majun <1075283428@qq.com>
2017-04-17 08:49:14 +0800

    20170417

---
+++ 20161007_08.20_interpreter.c
@@ -0,0 +1,22 @@
+#include "apue.h"
+#include "errors.h"
+#include <sys/wait.h>
+
+int
+main(void)
+{
+	pid_t pid;
+	
+	if((pid = fork()) < 0)
+	{
+		err_sys("fork error");
+	}
+	else if(pid == 0)
+	{
+		if(execl("/home/dmtsai/majun/github/apue/testinterp",(char *)0) < 0)
+			err_sys("execl error");
+	}
+	if(waitpid(pid,NULL,0) < 0)
+		err_sys("waitpid error");
+	exit(0);
+}
