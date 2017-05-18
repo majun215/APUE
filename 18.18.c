c73276c47e51c9bd1fff9e30ed1497c0bada1842
majun <1075283428@qq.com>
2017-05-18 16:02:24 +0800

    20170518

---
+++ 18.18.c
@@ -0,0 +1,16 @@
+#include "apue.h"
+
+char *getpass(const char *);
+
+int
+main(void)
+{
+	char *ptr;
+	if ((ptr = getpass("Enter password:")) == NULL)
+		err_sys("getpass error");
+	printf("password: %s\n", ptr);
+	/* now use password (probably encrypt it) ... */
+	while (*ptr != 0)
+		*ptr++ = 0; /* zero it out when we’re done with it */
+	exit(0);
+}
